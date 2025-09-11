7f8db1697b186a2a548bc36a49e318a5da267ff4
Aniket Yadav <aniketyadav690@gmail.com>
2025-09-11 11:56:04 +0530

    update: utils

--- src/utils/performanceUtils.ts
+++ src/utils/performanceUtils.ts
@@ -1,4 +1,6 @@
-import { type CalendarData } from "../types";
+/* eslint-disable prefer-spread */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { CalendarData, CalendarEvent } from "../types";
 
 export const createLazyDataLoader = (data: CalendarData) => {
   return {
@@ -14,6 +16,35 @@ export const createLazyDataLoader = (data: CalendarData) => {
       });
 
       return chunk;
+    },
+     // Virtualize events for large datasets
+    virtualizeEvents: (events: CalendarEvent[], viewStart: Date, viewEnd: Date): CalendarEvent[] => {
+      return events.filter(event => {
+        return event.start >= viewStart && event.start <= viewEnd;
+      });
+    },
+
+    // Debounce function for search/filter operations
+    debounce: <T extends (...args: any[]) => void>(func: T, delay: number): T => {
+      let timeoutId: NodeJS.Timeout;
+      return ((...args: any[]) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => func.apply(null, args), delay);
+      }) as T;
+    },
+
+    // Memoize 
+    memoize: <T extends (...args: any[]) => any>(fn: T): T => {
+      const cache = new Map();
+      return ((...args: any[]) => {
+        const key = JSON.stringify(args);
+        if (cache.has(key)) {
+          return cache.get(key);
+        }
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+      }) as T;
     },
   };
 };
